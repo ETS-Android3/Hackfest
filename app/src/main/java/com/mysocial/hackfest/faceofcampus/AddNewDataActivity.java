c432e97f020a5d3366ecf0edd29bc148e80966c2
Ansh <anshtandonat@gmail.com>
2022-05-07 14:40:05 +0530

    completed face of campus feature

---
+++ app/src/main/java/com/mysocial/hackfest/faceofcampus/AddNewDataActivity.java
@@ -0,0 +1,19 @@
+package com.mysocial.hackfest.faceofcampus;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mysocial.hackfest.R;
+
+public class AddNewDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_data_activtiy);
+
+
+
+    }
+}
